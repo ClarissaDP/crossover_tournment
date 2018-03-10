191092688dbc736741b1fdc2f0552c2d72879f5a
ClarissaDP <clarissa.d.pereira@gmail.com>
2018-03-10 10:50:37 -0300

    Adding base directory for codes

---
+++ answers/question_51.py
@@ -0,0 +1,15 @@
+
+
+def rearrange(elements):
+
+    size = elements[0] + 1
+    binaries = [size]
+
+    for i in range(1, size):
+        binary = bin(elements[i]).
+        binaries.append (bin(elements[i]))
+
+    print binaries
+
+rearrange([3,1,2,3])
+
